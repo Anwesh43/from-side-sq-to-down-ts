8cee4554fb16a853cbc0fcf2fa2474e9b79aeb62
Anwesh43 <anweshthecool0@gmail.com>
2021-09-04 17:27:11 +0530

    adding FromSquareSideToDown class

--- index.ts
+++ index.ts
@@ -168,3 +168,26 @@ class FSSTDNode {
         return this 
     }
 }
+
+class FromSideSquareToDown {
+
+    curr : FSSTDNode = new FSSTDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
