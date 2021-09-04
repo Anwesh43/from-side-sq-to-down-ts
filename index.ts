4c3f1b73469a5dcc756e9dbe35898f7b72b8a7a3
Anwesh43 <anweshthecool0@gmail.com>
2021-09-04 17:10:54 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -23,3 +23,31 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n))
     }
 }
+
+class DrawingUtil {
+
+    static drawFromSideSquareToDown(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor
+        const x : number = w / 2 - size / 2 * (1 - (sc2 - sc3))
+        const y  : number = (h / 2 + size / 2) * (-1 + sc1 + sc4) 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.translate(x, y)
+            context.fillRect(-size / 2, -size / 2, size , size)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawDFSSTDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawFromSideSquareToDown(context, scale)
+    }
+}
